28dd394bb2df35ccf8344d25a5b448b83e276ee3
Emily Spradling <spradling.ed@gmail.com>
2022-12-21 10:56:22 -0600

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ import os
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
